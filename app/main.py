2aac5710b0109f96ac644adb9c0ddf6541389a58
Yash Singh <yashsi3007@gmail.com>
2022-09-29 22:56:46 +0530

    Add enviorment variables
    Created a config.py file for accessing all enviorment variables.

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth
+from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
+
+print(settings.DATABASE_HOSTNAME)
 
 app = FastAPI()
 
